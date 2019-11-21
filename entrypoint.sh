50697eecb96c5268852fc9141fc2979f12a6570b
Youri op 't Roodt <yoptroodt@tripactions.com>
2019-11-21 11:01:31 +0100

    hack out superfluous newlines

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,52 +2,12 @@
 
 lint() {
   awk '
-    function report_blank(first, last) {
-      if (first == last) {
-        print "  " first ": superfluous blank line"
-      } else {
-        print "  " first "-" last ": superfluous blank lines"
-      }
-    }
-
     function report_trailing(line) {
       print "  " line ": trailing whitespace"
     }
 
-    BEGIN {
-      first_blank = 1
-      last_blank = 0
-    }
-
-    NF == 0 {
-      if (first_blank == 0) {
-        first_blank = NR
-      } else {
-        last_blank = NR
-      }
-    }
-
-    NF > 0 {
-      if (last_blank > 0) {
-        if (first_blank > 1) {
-          first_blank += 1
-        }
-        report_blank(first_blank, last_blank)
-      }
-      first_blank = last_blank = 0
-    }
-
     /[[:space:]]$/ {
       report_trailing(NR)
-    }
-
-    END {
-      if (first_blank > 0) {
-        if (last_blank == 0) {
-          last_blank = first_blank
-        }
-        report_blank(first_blank, last_blank)
-      }
     }
   ' "${1}"
 }
